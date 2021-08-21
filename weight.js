69371ea508722250abd547dc84e1608517bc3638
Ganesh <ganuofficial2795@gmail.com>
2021-08-21 11:45:23 +0530

    Weight-converted

--- weight.js
+++ weight.js
@@ -1,1 +1,33 @@
 console.log("Weight-Converter Using Javascript 🚀")
+
+let kilogram = document.querySelector("#Kilogram");
+let converter = document.querySelector("#Convert");
+let card = document.querySelector("#card");
+// console.log(kilogram,converter)
+
+converter.addEventListener("click",function(){
+
+    kg_value = kilogram.value;
+    
+    if(kg_value===null || kg_value===""){
+        alert("Kilogram value is missing. ")
+    }else{
+        
+        showConvertedWeights(kg_value)
+    }
+    kilogram.value=""
+})
+
+function showConvertedWeights(kg_units){
+
+    let createdDiv = document.querySelector("#box");
+    let html = ""
+    
+    html += `<ul class="list-group list-group-flush">
+            <li class="list-group-item"><h4>Kilograms(Entered):${kg_units}(kg) </h4></li>
+            <li class="list-group-item"><h4>In Grams: ${kg_units * 1000}(g)</h4></li>
+            <li class="list-group-item"><h4>In Pounds: ${kg_units * 2.2046}(lb)</h4></li>
+            <li class="list-group-item"><h4>In Ounces: ${kg_units * 35.2739}(oz)</h4></li>
+        </ul>`;
+    createdDiv.innerHTML = html;
+}
